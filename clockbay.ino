0a839ef7b0feae51bff176bdbfc357a119b5ee25
Zack Scholl <zack.scholl@gmail.com>
2021-02-04 06:59:55 -0800

    works better

--- clockbay.ino
+++ clockbay.ino
@@ -9,28 +9,32 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 // Analog 5  SCK
 
 // read the beat using A0
-int input_clock_analog_pin = A0;
-// subdivide the beat based on which digital pin you output from
-// 0 = send every pulse
-// 1 = send every other pulse
-// 2 = send every 2 pulses
-// 3 = send every 3 pulses
-int output_clock_digital_pin[] = {7, 8, 9, 10, 11};
+int input_setPulse = A0;
+int input_setVoltage = A1;
+int input_clock_analog_pin = A2;
+int output_clock_digital_pin = 7;
 
 // state
+// setPulse
+// 0 = no pulse
+// 1 = pulse 4x incoming
+// 2 = pulse 2x incoming
+// 3 = pulse at every incoming
+// 4 = pulse every other
+// 5 = pulse every other other
+int setPulse = 3;
+float setVoltage = 1;
 bool inputClockOn = false;
 unsigned long startClockTime = 0;
 float numberOfPulses = 0;
+unsigned long lastVoltageSet = 0;
 unsigned long lastPulseStart = 0;
 unsigned long lastPulseDuration = 0;
 unsigned long durationBetweenLastPulses = 0;
 bool inDoublePulse = false;
 
 void setup() {
-  // set subdivided output clocks
-  for (int i = 0; i < (sizeof(output_clock_digital_pin) / sizeof(output_clock_digital_pin[0])); i++) {
-    pinMode(output_clock_digital_pin[i], OUTPUT);
-  }
+  pinMode(output_clock_digital_pin, OUTPUT);
 
   // set font
   u8g.setFont(u8g_font_unifont);
@@ -60,20 +64,30 @@ void reset_timer() {
 }
 
 void loop() {
+  // read potentiometer states
+  int val = 0;
+  unsigned long currentTime = millis();
+
+  // get how to do current pulse
+  setPulse = map(analogRead(input_setPulse), 0, 1023, 0, 5);
+
+  // read what the current voltage is set to
+  // by measuring the max value over time
+  float new_voltage = (float)analogRead(input_setVoltage) / 1023.0 * 5.0;
+  if (new_voltage > 0 && (new_voltage > setVoltage || currentTime - lastVoltageSet > 1000)) {
+    setVoltage = new_voltage;
+    lastVoltageSet = currentTime;
+  }
+
   bool inputClockOn_old = inputClockOn;
-  int val = analogRead(input_clock_analog_pin);
+  val = analogRead(input_clock_analog_pin);
   inputClockOn = (val > 50); // > 0.25 volts
-  //  if (val > 0) {
-  //    Serial.println(val);
-  //  }
   if (inputClockOn != inputClockOn_old) {
     if (inputClockOn) {
       if (lastPulseStart > 0) {
-        durationBetweenLastPulses = millis() - lastPulseStart;
-      Serial.print("durationBetweenLastPulses: ");
-      Serial.println(durationBetweenLastPulses);
+        durationBetweenLastPulses = currentTime - lastPulseStart;
       }
-      lastPulseStart = millis();
+      lastPulseStart = currentTime;
       if (startClockTime == 0) {
         startClockTime = lastPulseStart;
       } else {
@@ -82,42 +96,88 @@ void loop() {
         struct bpmResult result = calculateBPM(numberOfPulses, (float)(lastPulseStart - startClockTime));
         Serial.print("bpm = ");
         Serial.print(result.bpm);
+        Serial.print(", setPulse = ");
+        Serial.print(setPulse);
+        Serial.print(", setVoltage = ");
+        Serial.print(setVoltage);
+        Serial.print(", lastPulseDuration = ");
+        Serial.print(lastPulseDuration);
+        Serial.print(", durationBetweenLastPulses = ");
+        Serial.print(durationBetweenLastPulses);
         Serial.print(", divisions = ");
         Serial.println(result.divisions);
-        u8g.drawStr(20, 40, "Hello World."); // TODO show bpm
+        //        u8g.drawStr(20, 40, "Hello World."); // TODO show bpm
       }
     } else {
-      lastPulseDuration =millis() - lastPulseStart;
+      lastPulseDuration = currentTime - lastPulseStart;
     }
-    // write to the clock for each subdivision
-    if (inputClockOn) {
-      digitalWrite(13, HIGH);
-      digitalWrite(output_clock_digital_pin[0], HIGH);
-      for (int i = 1; i < (sizeof(output_clock_digital_pin) / sizeof(output_clock_digital_pin[0])); i++) {
-        if ((int)numberOfPulses % (i + 1 ) == 0) {
-          digitalWrite(output_clock_digital_pin[i], HIGH);
-        }
+    if (setPulse > 0) {
+      // write to the clock for each subdivision
+      bool on = false;
+      if (setPulse > 0 && setPulse <= 3) {
+        on = true;
+      } else if (setPulse == 4 && (int)numberOfPulses % 2 == 0) {
+        on = true;
+      } else if (setPulse == 5 && (int)numberOfPulses % 4 == 0) {
+        on = true;
       }
-    } else {
-      digitalWrite(13, LOW);
-      digitalWrite(output_clock_digital_pin[0], HIGH);
-      for (int i = 1; i < (sizeof(output_clock_digital_pin) / sizeof(output_clock_digital_pin[0])); i++) {
-        digitalWrite(output_clock_digital_pin[i], LOW);
+      if (inputClockOn && on) {
+        Serial.println("pulse on");
+        digitalWrite(13, HIGH);
+        digitalWrite(output_clock_digital_pin, HIGH);
+        inDoublePulse = true;
+      } else if (inDoublePulse) {
+        inDoublePulse = false;
+        Serial.println("pulse off");
+        digitalWrite(13, LOW);
+        digitalWrite(output_clock_digital_pin, HIGH);
       }
     }
   }
 
-  // write the double pulse time
-  if (durationBetweenLastPulses > 0) {
+  // stop main pulse
+  if (currentTime - lastPulseStart > 0 && currentTime - lastPulseStart < durationBetweenLastPulses * 1 / 4 && currentTime - lastPulseStart > lastPulseDuration / 5 && inDoublePulse) {
+    inDoublePulse = false;
+    Serial.println("pulse off");
+    digitalWrite(13, LOW);
+    digitalWrite(output_clock_digital_pin, HIGH);
+  }
+
+  //  // write the double pulse time
+  if (durationBetweenLastPulses > 0 && setPulse > 0 && setPulse < 3) {
+    int iStart = 2;
+    int iEnd = 2;
+    if (setPulse == 1) {
+      iStart = 1;
+      iEnd = 3;
+    }
     if (inDoublePulse == false) {
-      if (millis() - lastPulseStart > durationBetweenLastPulses / 2 & millis() - lastPulseStart < durationBetweenLastPulses / 2 + lastPulseDuration/2) {
-        inDoublePulse = true;
-        digitalWrite(output_clock_digital_pin[0], HIGH);
+      for (int i = iStart; i <= iEnd; i++) {
+        if (currentTime - lastPulseStart >= durationBetweenLastPulses * (i + 1) / 4) {
+          continue;
+        }
+        if (currentTime - lastPulseStart > durationBetweenLastPulses * i / 4 & currentTime - lastPulseStart < durationBetweenLastPulses * i / 4 + lastPulseDuration / 5) {
+          inDoublePulse = true;
+          Serial.print("pulse");
+          Serial.print(i);
+          Serial.println(" on");
+          digitalWrite(13, HIGH);
+          digitalWrite(output_clock_digital_pin, HIGH);
+        }
       }
     } else {
-      if (millis() - lastPulseStart > durationBetweenLastPulses / 2 + lastPulseDuration / 2) {
-        inDoublePulse = false;
-        digitalWrite(output_clock_digital_pin[0], LOW);
+      for (int i = iStart; i <= iEnd; i++) {
+        if (currentTime - lastPulseStart >= durationBetweenLastPulses * (i + 1) / 4) {
+          continue;
+        }
+        if (currentTime - lastPulseStart > durationBetweenLastPulses * i / 4 + lastPulseDuration / 5) {
+          inDoublePulse = false;
+          Serial.print("pulse");
+          Serial.print(i);
+          Serial.println(" off");
+          digitalWrite(13, LOW);
+          digitalWrite(output_clock_digital_pin, LOW);
+        }
       }
     }
   }
